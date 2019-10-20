d7b2c8431da2bf1fc72ed7d2d80b1c0bbd6ab78e
fanzhongqiu <56763841+fanzhongqiu@users.noreply.github.com>
2019-10-20 18:41:17 +0800

    Add files via upload

---
+++ 27.py
@@ -0,0 +1,61 @@
+# Python3 命名空间和作用域
+#命名空间的生命周期：
+# var1 是全局名称
+var1 = 5
+
+
+def some_func():
+    # var2 是局部名称
+    var2 = 6
+
+    def some_inner_func():
+        # var3 是内嵌的局部名称
+        var3 = 7
+#全局变量和局部变量
+# !/usr/bin/python3
+
+total = 0  # 这是一个全局变量
+
+
+# 可写函数说明
+def sum(arg1, arg2):
+    # 返回2个参数的和."
+    total = arg1 + arg2  # total在这里是局部变量.
+    print("函数内是局部变量 : ", total)
+    return total
+
+
+# 调用sum函数
+sum(10, 20)
+print("函数外是全局变量 : ", total)
+#global 和 nonlocal关键字
+# !/usr/bin/python3
+
+num = 1
+
+
+def fun1():
+    global num  # 需要使用 global 关键字声明
+    print(num)
+    num = 123
+    print(num)
+
+
+fun1()
+print(num)
+#如果要修改嵌套作用域（enclosing 作用域，外层非全局作用域）中的变量则需要 nonlocal 关键字了，如下实例：
+# !/usr/bin/python3
+
+def outer():
+    num = 10
+
+    def inner():
+        nonlocal num  # nonlocal关键字声明
+        num = 100
+        print(num)
+
+    inner()
+    print(num)
+
+
+outer()
---
+++ 28.py
@@ -0,0 +1,114 @@
+#Python3 标准库概览
+#操作系统接口
+>>> import os
+>>> os.getcwd()      # 返回当前的工作目录
+'C:\\Python34'
+>>> os.chdir('/server/accesslogs')   # 修改当前的工作目录
+>>> os.system('mkdir today')   # 执行系统命令 mkdir
+0
+#文件通配符
+>>> import glob
+>>> glob.glob('*.py')
+['primes.py', 'random.py', 'quote.py']
+#命令行参数
+>>> import sys
+>>> print(sys.argv)
+['demo.py', 'one', 'two', 'three']
+#错误输出重定向和程序终止
+>>> sys.stderr.write('Warning, log file not found starting a new one\n')
+Warning, log file not found starting a new one
+#字符串正则匹配
+>>> import re
+>>> re.findall(r'\bf[a-z]*', 'which foot or hand fell fastest')
+['foot', 'fell', 'fastest']
+>>> re.sub(r'(\b[a-z]+) \1', r'\1', 'cat in the the hat')
+'cat in the hat'
+#数学
+>> import math
+>>> math.cos(math.pi / 4)
+0.70710678118654757
+>>> math.log(1024, 2)
+10.0
+#random提供了生成随机数的工具。
+>>> import random
+>>> random.choice(['apple', 'pear', 'banana'])
+'apple'
+>>> random.sample(range(100), 10)   # sampling without replacement
+[30, 83, 16, 4, 8, 81, 41, 50, 18, 33]
+>>> random.random()    # random float
+0.17970987693706186
+>>> random.randrange(6)    # random integer chosen from range(6)
+4
+#访问 互联网
+>>> from urllib.request import urlopen
+>>> for line in urlopen('http://tycho.usno.navy.mil/cgi-bin/timer.pl'):
+...     line = line.decode('utf-8')  # Decoding the binary data to text.
+...     if 'EST' in line or 'EDT' in line:  # look for Eastern Time
+...         print(line)
+
+<BR>Nov. 25, 09:43:32 PM EST
+
+>>> import smtplib
+>>> server = smtplib.SMTP('localhost')
+>>> server.sendmail('soothsayer@example.org', 'jcaesar@example.org',
+... """To: jcaesar@example.org
+... From: soothsayer@example.org
+...
+... Beware the Ides of March.
+... """)
+>>> server.quit()
+#日期和时间
+>>> # dates are easily constructed and formatted
+>>> from datetime import date
+>>> now = date.today()
+>>> now
+datetime.date(2003, 12, 2)
+>>> now.strftime("%m-%d-%y. %d %b %Y is a %A on the %d day of %B.")
+'12-02-03. 02 Dec 2003 is a Tuesday on the 02 day of December.'
+
+>>> # dates support calendar arithmetic
+>>> birthday = date(1964, 7, 31)
+>>> age = now - birthday
+>>> age.days
+14368
+#数据压缩
+>>> import zlib
+>>> s = b'witch which has which witches wrist watch'
+>>> len(s)
+41
+>>> t = zlib.compress(s)
+>>> len(t)
+37
+>>> zlib.decompress(t)
+b'witch which has which witches wrist watch'
+>>> zlib.crc32(s)
+226805979
+#性能度量
+>>> from timeit import Timer
+>>> Timer('t=a; a=b; b=t', 'a=1; b=2').timeit()
+0.57535828626024577
+>>> Timer('a,b = b,a', 'a=1; b=2').timeit()
+0.54962537085770791
+#测试模块
+def average(values):
+    """Computes the arithmetic mean of a list of numbers.
+
+    >>> print(average([20, 30, 70]))
+    40.0
+    """
+    return sum(values) / len(values)
+
+import doctest
+doctest.testmod()   # 自动验证嵌入测试
+#unittest模块不像 doctest模块那么容易使用，不过它可以在一个独立的文件里提供一个更全面的测试集:
+import unittest
+
+class TestStatisticalFunctions(unittest.TestCase):
+
+    def test_average(self):
+        self.assertEqual(average([20, 30, 70]), 40.0)
+        self.assertEqual(round(average([1, 5, 7]), 1), 4.3)
+        self.assertRaises(ZeroDivisionError, average, [])
+        self.assertRaises(TypeError, average, 20, 30, 70)
+
+unittest.main() # Calling from the command line invokes all tests
